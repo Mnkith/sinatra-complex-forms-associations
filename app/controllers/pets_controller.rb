d45d2a05f32015632db44537f0756d07f8b026da
Mnkith <munkithabid@gmail.com>
2021-01-23 17:08:07 -0600

    commit tree bf80cae26c3ccc5c6bb39dbc2788a1e195ed174c

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,11 +6,20 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
+    @pet = Pet.find_or_create_by(name: params[:pet][:name])
+    # binding.pry
+    if params[:pet][:owner_id]
+      @pet.owner = Owner.find(params[:pet][:owner_id])
+    elsif params[:pet][:owner_name] != ""
+      @pet.owner = Owner.create(name: params[:pet][:owner_name])
+    end
+    @pet.save
+    # binding.pry
     redirect to "pets/#{@pet.id}"
   end
 
@@ -19,8 +28,23 @@ class PetsController < ApplicationController
     erb :'/pets/show'
   end
 
+  get '/pets/:id/edit' do
+    @pet = Pet.find params[:id]
+    @owners = Owner.all
+    # binding.pry
+    erb :'/pets/edit'
+  end
   patch '/pets/:id' do 
-
+    @pet = Pet.find params[:id]
+    @pet.name = params[:pet_name]
+    
+    if params[:owner][:name] != ""
+      @pet.owner = Owner.create(name: params[:owner][:name])
+    elsif params[:pet][:owner_id]
+      @pet.owner = Owner.find(params[:pet][:owner_id])
+    end
+    # binding.pry
+    @pet.save
     redirect to "pets/#{@pet.id}"
   end
 end
